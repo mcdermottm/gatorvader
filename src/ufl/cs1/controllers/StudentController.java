43eb11a49085548144598d237e89409dcbc42663
LAPTOP-2N5OIS3K\sasan <sasankdesaraju@gmail.com>
2017-12-03 13:33:22 -0500

    Changed behaviour based on fear state
    Tried to make protecc when attacker near power pill

--- src/ufl/cs1/controllers/StudentController.java
+++ src/ufl/cs1/controllers/StudentController.java
@@ -1,9 +1,7 @@
 package ufl.cs1.controllers;
 
 import game.controllers.DefenderController;
-import game.models.Defender;
-import game.models.Game;
-import game.models.Node;
+import game.models.*;
 
 import java.util.List;
 
@@ -35,6 +33,117 @@ public final class StudentController implements DefenderController
 	}
 
 	public int getBehavior1(Defender defender, Game game){
+        Maze maze = game.getCurMaze();
+	    Node attacker = game.getAttacker().getLocation();
+        Attacker _attacker = game.getAttacker();
+        List<Integer> directions = defender.getPossibleDirs();
+        List<Node> locations = defender.getPossibleLocations();
+        if (directions.size() > 1 && defender.getLairTime() <= 0){
+            int bestDirection = -1;
+            int bestLength = 1000;
+            for(int i=0; i<4; i++){
+                if(locations.get(i) != null){
+                    int possibleLength = locations.get(i).getPathDistance(attacker);
+                    Node closestPowerPill = _attacker.getTargetNode(maze.getPowerPillNodes(), true);
+                    if (!defender.isVulnerable()) {
+                        if (possibleLength < bestLength) {
+                            bestDirection = i;
+                            bestLength = possibleLength;
+                        }
+                    }
+                    else if (defender.isVulnerable()) {
+                        if (possibleLength > bestLength) {
+                            bestDirection = i;
+                            bestLength = possibleLength;
+                        }
+                    }
+                    else if (_attacker.getLocation().getPathDistance(closestPowerPill) <= 10) {
+                        if (possibleLength > bestLength) {
+                            bestDirection = i;
+                            bestLength = possibleLength;
+                        }
+                    }
+                }
+            }
+            return bestDirection;
+        }
+        return 1;
+    }
+    public int getBehavior2(Defender defender, Game game){
+        Maze maze = game.getCurMaze();
+        Node attacker = game.getAttacker().getLocation();
+        Attacker _attacker = game.getAttacker();
+        List<Integer> directions = defender.getPossibleDirs();
+        List<Node> locations = defender.getPossibleLocations();
+        if (directions.size() > 1 && defender.getLairTime() <= 0){
+            int bestDirection = -1;
+            int bestLength = 1000;
+            for(int i=0; i<4; i++){
+                if(locations.get(i) != null){
+                    int possibleLength = locations.get(i).getPathDistance(attacker);
+                    Node closestPowerPill = _attacker.getTargetNode(maze.getPowerPillNodes(), true);
+                    if (!defender.isVulnerable()) {
+                        if (possibleLength < bestLength) {
+                            bestDirection = i;
+                            bestLength = possibleLength;
+                        }
+                    }
+                    else if (defender.isVulnerable()) {
+                        if (possibleLength > bestLength) {
+                            bestDirection = i;
+                            bestLength = possibleLength;
+                        }
+                    }
+                    else if (_attacker.getLocation().getPathDistance(closestPowerPill) <= 10) {
+                        if (possibleLength > bestLength) {
+                            bestDirection = i;
+                            bestLength = possibleLength;
+                        }
+                    }
+                }
+            }
+            return bestDirection;
+        }
+        return 1;
+    }
+    public int getBehavior3(Defender defender, Game game){
+        Maze maze = game.getCurMaze();
+        Node attacker = game.getAttacker().getLocation();
+        Attacker _attacker = game.getAttacker();
+        List<Integer> directions = defender.getPossibleDirs();
+        List<Node> locations = defender.getPossibleLocations();
+        if (directions.size() > 1 && defender.getLairTime() <= 0){
+            int bestDirection = -1;
+            int bestLength = 1000;
+            for(int i=0; i<4; i++){
+                if(locations.get(i) != null){
+                    int possibleLength = locations.get(i).getPathDistance(attacker);
+                    Node closestPowerPill = _attacker.getTargetNode(maze.getPowerPillNodes(), true);
+                    if (!defender.isVulnerable()) {
+                        if (possibleLength < bestLength) {
+                            bestDirection = i;
+                            bestLength = possibleLength;
+                        }
+                    }
+                    else if (defender.isVulnerable()) {
+                        if (possibleLength > bestLength) {
+                            bestDirection = i;
+                            bestLength = possibleLength;
+                        }
+                    }
+                    else if (_attacker.getLocation().getPathDistance(closestPowerPill) <= 10) {
+                        if (possibleLength > bestLength) {
+                            bestDirection = i;
+                            bestLength = possibleLength;
+                        }
+                    }
+                }
+            }
+            return bestDirection;
+        }
+        return 1;
+    }
+    public int getBehavior4(Defender defender, Game game){
         Node attacker = game.getAttacker().getLocation();
         List<Integer> directions = defender.getPossibleDirs();
         List<Node> locations = defender.getPossibleLocations();
@@ -53,19 +162,5 @@ public final class StudentController implements DefenderController
             return bestDirection;
         }
         return 1;
-    }
-    public int getBehavior2(Defender defender, Game game){
-	    return 0;
-    }
-    public int getBehavior3(Defender defender, Game game){
-        return 0;
-    }
-    public int getBehavior4(Defender defender, Game game){
-        //Non-student code, just a placeholder for the fourth defender's behavior
-        List<Integer> possibleDirs = defender.getPossibleDirs();
-        if (possibleDirs.size() != 0)
-            return possibleDirs.get(Game.rng.nextInt(possibleDirs.size()));
-        else
-            return -1;
     }
 }
